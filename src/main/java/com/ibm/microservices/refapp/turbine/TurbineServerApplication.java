3c42ed6d4a61c1e6026a5fb50f6972569b69ce5a
jkwong888 <jkwong@ca.ibm.com>
2017-10-03 10:04:13 -0400

    update chart for umbrella

--- src/main/java/com/ibm/microservices/refapp/turbine/TurbineServerApplication.java
+++ src/main/java/com/ibm/microservices/refapp/turbine/TurbineServerApplication.java
@@ -3,11 +3,11 @@ package com.ibm.microservices.refapp.turbine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableTurbine
+@EnableTurbineStream
 
 public class TurbineServerApplication {
 
